4a8d5ff7dd9f31810bcb374bee2322003294fca5
danielboe <danielboeber@gmail.com>
2023-08-11 00:12:53 +0200

    feat(parse) add download via fetch WIP feat(main) prepare for fetch download WIP

--- app.mjs
+++ app.mjs
@@ -2,55 +2,51 @@ import express from "express";
 import serveIndex from "serve-index";
 import { configparser } from "./helpers/configparser.mjs";
 import path from "path";
+import { config } from "dotenv";
+config();
 
-const app = express();
 const __dirname = path.dirname(new URL(import.meta.url).pathname);
 
 //const configparser = require("./helpers/configparser.mjs");
 
-const baseurl = "https://exhibition-strapi.herokuapp.com/devices/12";
+const BASEURL = "https://preview-phi.vercel.app/api/download";
+const SLUG = "beispiel";
 
-var serverport = 3000;
 //Download Dir, URL für Config, isDebug?
 async function main() {
-  var timeout = configparser.check_env_var("SYNCTIMEOUT", 5000); //Timeout for sleep between sync checks
-
-  configparser.init(__dirname, baseurl, false).then(function () {
-    download(timeout).then((ok) => {
-      serverport = configparser.check_env_var("SERVERPORT", serverport); //Server port for Node server
-      app.use("/", express.static(configparser.get_content_dir()), serveIndex(configparser.get_content_dir(), { icons: true }));
-      //app.use(express.static(configparser.get_content_dir()));
-      app.listen(serverport, () => console.log("[SERVER] static server on port: " + serverport));
-    });
-  });
+  let timeout = configparser.check_env_var("SYNCTIMEOUT", 5000); //Timeout for sleep between sync checks
+  let serverport = configparser.check_env_var("SERVERPORT", 3000); // port for express server
+  let TOKEN = configparser.check_env_var("TOKEN", "notforyou", true);
+  var baseUrl = BASEURL + "?slug=" + SLUG + "&token=" + TOKEN;
+  await configparser.init(__dirname, baseUrl, true);
+  await download(timeout);
+  //TODO check if we want a success download before start server
+  //const app = express();
+  //app.use("/", express.static(configparser.get_content_dir()), serveIndex(configparser.get_content_dir(), { icons: true }));
+  //app.listen(serverport, () => console.log("[SERVER] start file server on port: " + serverport));
 }
 
 main();
 
 function download(timeout) {
   return new Promise((resolve, reject) => {
-    function downloadLoop() {
-      configparser
-        .parseUrls()
-        .then((urls) => {
-          configparser.download(urls).then((isDownload) => {
-            if (isDownload) {
-              configparser.sync();
-              if (isDownload != "sync") configparser.clean();
-              resolve(true);
-              setTimeout(downloadLoop, timeout);
-            }
-          });
-        })
-        .catch((err) => {
-          console.error(err);
-          console.error("[MAIN] ERR try to restart ....");
+    async function downloadLoop() {
+      try {
+        let parsedFile = await configparser.parseUrls();
+        let downloadSuccess = await configparser.download(parsedFile.fetchData, parsedFile.configFile);
+        if (downloadSuccess) {
+          configparser.sync(); //TODO make this async?
+          if (downloadSuccess != "sync") configparser.clean(parsedFile.fetchData, parsedFile.configFile);
           resolve(true);
-          setTimeout(downloadLoop, timeout);
-        });
+        }
+        setTimeout(downloadLoop, timeout);
+      } catch (error) {
+        console.error(error);
+        console.error("[MAIN] ERR try to restart ....");
+        resolve(false);
+        setTimeout(downloadLoop, timeout);
+      }
     }
     downloadLoop();
-
-    //setTimeout(arguments.callee, 3000);
   });
 }
--- helpers/configparser.mjs
+++ helpers/configparser.mjs
@@ -1,21 +1,14 @@
-import http from "http";
-import getUrls from "get-urls";
+import https from "https";
 import url from "url";
 import fs from "fs";
 import path from "path";
 import async from "async";
-import cliProgress from "cli-progress";
+import fetch from "node-fetch";
 
 var URL_IGNORES = [];
-const bars = [];
-var bars_multi;
 
 var UPDATE_FOLDER = "";
 var LIVE_FOLDER = "";
-var AUTH_USER = "";
-var AUTH_PW = "";
-var ParsedFiles;
-var ConfigJSON;
 var BASEPATH; //Base of the running app (absolut)
 var BASE_URL;
 var ISDEBUG = false;
@@ -81,7 +74,7 @@ function copyFolderRecursiveSync(source, target) {
       if (fs.lstatSync(curSource).isDirectory()) {
         copyFolderRecursiveSync(curSource, targetFolder);
       } else {
-        console.log("[FILES] Sync: " + curSource + "  To: " + targetFolder + "/" + file);
+        console.log("[SYNC] source: " + curSource + "  to: " + targetFolder + "/" + file);
         copyFileSync(curSource, targetFolder);
       }
     });
@@ -127,13 +120,47 @@ var listFilesRecursive = function (dir, done) {
     })();
   });
 };
-const downloadFile = async function (url_dl, cli_index = 0, cb = null) {
+
+const downloadHttps = async function (inputUrl) {
   return new Promise((resolve, reject) => {
-    var dest = url.parse(url_dl).pathname;
-    var dest_string = dest.indexOf("/") == 0 ? dest.substring(1) : dest;
-    if (!fs.existsSync(LIVE_FOLDER + "/" + dest_string) && !fs.existsSync(UPDATE_FOLDER + "/" + dest_string)) {
-      //console.log("Download URL: " + url_dl);
-      var folders = dest_string.split("/");
+    const reqUrl = new URL(inputUrl);
+    var request = https.request(
+      {
+        path: reqUrl.pathname + reqUrl.search,
+        hostname: reqUrl.hostname,
+      },
+      function (response) {
+        if (response.statusCode < 200 || response.statusCode > 299) {
+          reject("[DLURL] ERR failed to load page, status code: " + response.statusCode);
+          return;
+        }
+        response.setEncoding("utf8");
+        var data = "";
+        response.on("data", function (chunk) {
+          data += chunk;
+        });
+        response.on("end", () => {
+          resolve(data);
+        });
+      }
+    );
+    request.on("error", (err) => {
+      reject("[DLURL] ERR request error " + err);
+    });
+    request.end();
+  });
+};
+
+const downloadFile = async function (fetchData, cli_index = 0, cb = null) {
+  return new Promise(async (resolve, reject) => {
+    if (!fetchData.hasOwnProperty("path")) {
+      console.log("[DOWNLOADER] No Path provided for Element: " + cli_index);
+      resolve(false);
+    }
+    var destination = fetchData.path;
+    var fetchRequest = fetchData.fetch;
+    if (!fs.existsSync(LIVE_FOLDER + "/" + destination) && !fs.existsSync(UPDATE_FOLDER + "/" + destination)) {
+      var folders = destination.split("/");
       var j;
       var struct = UPDATE_FOLDER + "/";
       if (!fs.existsSync(struct)) {
@@ -150,55 +177,29 @@ const downloadFile = async function (url_dl, cli_index = 0, cb = null) {
           fs.mkdirSync(struct);
         }
       }
-      struct = "";
-      var file = fs.createWriteStream(UPDATE_FOLDER + "/" + dest_string);
-      var request = http
-        .get(
-          {
-            path: url.parse(url_dl).pathname,
-            hostname: url.parse(url_dl).hostname,
-            //auth: AUTH_USER + ":" + AUTH_PW,
-          },
-          function (response) {
-            if (response.statusCode < 200 || response.statusCode > 299) {
-              //console.log("Error Code:" + response.statusCode + " URL: " + url_dl);
-              fs.unlink(UPDATE_FOLDER + "/" + dest_string, function () {
-                console.log("[DOWNLOADER] ERR code:" + response.statusCode + " del file: " + UPDATE_FOLDER + "/" + dest_string);
-              });
-            }
-            response.pipe(file);
-
-            var download_size = Math.round(response.headers["content-length"] / 1048576);
-            bars[cli_index] = bars_multi.create(download_size, 0, { filename: dest_string });
-            //console.log(" " + url_dl + ` (${download_size} MB)`);
-            var download_log = setInterval(function () {
-              var stats = fs.statSync(UPDATE_FOLDER + "/" + dest_string);
-              var size = Math.round(stats.size / 1048576);
-              bars[cli_index].update(size, { filename: dest_string });
-              //printSameline("Status: " + Math.round((size / download_size) * 100) + "% (" + size + " MB)");
-              //console.log("Status: " + Math.round((size / download_size) * 100) + "% (" + size + " MB)");
-            }, 2000);
-            file.on("finish", function () {
-              clearInterval(download_log);
-              bars[cli_index].update(download_size, { filename: dest_string });
-              bars[cli_index].stop();
-              //console.log("Download Finished URL: " + url_dl);
-              file.close(resolve(true));
-              //file.close(cb); // close() is async, call cb after close completes.
-            });
-          }
-        )
-        .on("error", function (err) {
-          // Handle errors
-          console.log("[DOWNLOADER] ERR: " + err);
-          fs.unlink(UPDATE_FOLDER + "/" + dest_string, function () {
-            console.log("[DOWNLOADER] ERR:");
-            //if (cb) cb(err.message);
-            resolve(err.message);
-          }); // Delete the file async. (But we don't check the result)
+      const res = await fetch(fetchRequest.url, fetchRequest.request);
+      const fileStream = fs.createWriteStream(UPDATE_FOLDER + "/" + destination, { flags: "wx" });
+      let fetchResult = await new Promise((resolve, reject) => {
+        console.log("[DOWNLOADER] Downloading: " + destination + " - ");
+        let downloadedBytes = 0;
+        const totalBytes = res.headers.get("content-length");
+        res.body.pipe(fileStream);
+        res.body.on("data", (chunk) => {
+          downloadedBytes += chunk.length;
+          const progress = Math.round((downloadedBytes / totalBytes) * 100);
+          process.stdout.write(`Downloaded ${progress}%\r`);
         });
+        res.body.on("error", function (err) {
+          console.log("[DOWNLOADER] ERR fetch: " + err);
+          resolve(false);
+        });
+        fileStream.on("finish", resolve);
+      });
+      //URL_IGNORES TODO: add files that filed multible times to ignore list
+
+      resolve(true);
     } else {
-      if (ISDEBUG) console.log("[DOWNLOADER] file exists");
+      if (ISDEBUG) console.log("[DOWNLOADER] file exists: " + destination);
       resolve(false);
       //cb(null);
     }
@@ -243,169 +244,98 @@ function checkENV(ENV, alt_var, secret = false) {
 
 export const configparser = {
   //Get all Download links from json
-  init: function (
-    basepath,
-    baseurl,
-    isdebug = false,
-    l_folder = "update_live",
-    u_folder = "update_tmp",
-    auth_user = "ausstellung",
-    auth_pw = "osb23nmp"
-  ) {
+  init: function (basepath, baseurl, isdebug = false, l_folder = "update_live", u_folder = "update_tmp") {
     return new Promise((resolve, reject) => {
       console.log("---INIT FILESYNC---");
       BASEPATH = checkENV("BASEPATH", basepath); //Path to the directory for downloads (app dir default)
-      AUTH_USER = checkENV("AUTH_USER", auth_user); //auth user for download
-      AUTH_PW = checkENV("AUTH_PW", auth_pw, true); //auth pw for download
       UPDATE_FOLDER = checkENV("UPDATE_FOLDER", u_folder); //folder to save files in update
       LIVE_FOLDER = checkENV("LIVE_FOLDER", l_folder); //folder to save files when download done
-      BASE_URL = checkENV("BASE_URL", baseurl); //url to config json with files
+      BASE_URL = checkENV("BASE_URL", baseurl, true); //url to config json with files
       ISDEBUG = checkBool(checkENV("ISDEBUG", isdebug)); //enable debug for console output
       deleteFolderRecursive(UPDATE_FOLDER);
-
-      bars_multi = new cliProgress.MultiBar(
-        {
-          clearOnComplete: false,
-          hideCursor: true,
-          format: "                                        {bar} {percentage}%  | ETA: {eta}s | {value}/{total} MB | {filename}",
-        },
-        cliProgress.Presets.shades_grey
-      );
-
       console.log("---INIT DONE--- ");
       resolve(true);
     });
   },
   //ToDo: Clean JSON from not downloadable URLs
-  parseUrls: function (address = BASE_URL) {
-    return new Promise((resolve, reject) => {
-      var request = http.request(
-        {
-          path: url.parse(address).pathname,
-          hostname: url.parse(address).hostname,
-          //auth: AUTH_USER + ":" + AUTH_PW,
-        },
-        function (response) {
-          if (response.statusCode < 200 || response.statusCode > 299) {
-            reject("[PARSE] ERR failed to load page, status code: " + response.statusCode);
-          }
-          response.setEncoding("utf8");
-          let urls_data;
-          var data = "";
-          response.on("data", function (chunk) {
-            data += chunk;
-          });
-          response.on("end", () => {
-            let json = IsJsonString(data);
-            //console.log("Session: %j", json);
-            if (json == false) {
-              reject("[PARSE] ERR response is no valid json");
-            }
-            let text = JSON.stringify(json);
-            ConfigJSON = text;
-            if (ISDEBUG) console.log("[PARSE] Excluded URLs");
-            if (ISDEBUG) console.log(URL_IGNORES);
-            urls_data = Array.from(getUrls(text, { requireSchemeOrWww: true, exclude: URL_IGNORES }));
-            URL_IGNORES.forEach((element) => {
-              if (urls_data.indexOf(element) > -1) {
-                let index = urls_data.indexOf(element);
-                if (ISDEBUG) console.log("Found index in Output " + index);
-                urls_data.splice(index, index + 1);
-                //In the array!
-              }
-            });
 
-            if (ISDEBUG) console.log("[PARSE] Left for DL URLs");
-            if (ISDEBUG) console.log(urls_data);
-            //Filter URLS that are not wanted (no files)
-            ParsedFiles = urls_data;
-            resolve(urls_data);
-          });
-        }
-      );
-      request.on("error", (err) => {
-        reject(err);
-      });
-      request.end();
+  parseUrls: function (address = BASE_URL) {
+    return new Promise(async (resolve, reject) => {
+      let downloadData = await downloadHttps(address);
+
+      let responseJson = IsJsonString(downloadData);
+      //console.log("[PARSE] Session: %j", json);
+      if (responseJson == false) {
+        reject("[PARSE] ERR response is no valid json");
+        return;
+      }
+      let rawData = JSON.stringify(responseJson);
+      let fetchData = responseJson.data;
+      //URL_IGNORES TODO: check if urls not work and exclude from array of fetch data
+      if (ISDEBUG) console.log("[PARSE] " + fetchData.length + " urls found");
+      resolve({ fetchData, configFile: rawData });
     });
   },
-  //Download URL to Temp Dir
-  download: async function (urls) {
-    return new Promise((resolve, reject) => {
-      if (ISDEBUG) console.log("--DOWNLOAD " + urls.length + " FILES--");
-      if (ISDEBUG) console.log(urls);
-      //console.log(fs.readFileSync(BASEPATH + '/' + LIVE_FOLDER + "/config.json") == ConfigJSON)
-      var configSync = false;
+
+  //Download Configs to Temp Dir
+  download: async function (fetchData, configFile) {
+    return new Promise(async (resolve, reject) => {
+      let configSync = false;
       if (fs.existsSync(BASEPATH + "/" + LIVE_FOLDER + "/config.json")) {
-        if (fs.readFileSync(BASEPATH + "/" + LIVE_FOLDER + "/config.json") == ConfigJSON) {
+        if (fs.readFileSync(BASEPATH + "/" + LIVE_FOLDER + "/config.json") == configFile) {
           configSync = true;
-          //resolve("sync");
-          //return;
+          resolve("sync");
+          return;
         } else {
-          console.log("[DOWNLOAD] Config Update");
           configSync = false;
         }
-        if (ISDEBUG) console.log("[DOWNLOAD] configSync: " + configSync);
+        if (ISDEBUG) console.log("[DOWNLOAD] config updated: " + configSync);
       }
-      if (typeof urls === "undefined" && !urls.length > 0) {
-        console.log("[DOWNLOAD] No URLs to Download");
-        resolve(false);
-      }
-      newdl_counter = 0;
-      async
-        .forEachOfLimit(urls, 5, (value, key, callback) => {
+      if (ISDEBUG) console.log("[DOWNLOAD] " + fetchData.length + " files to download");
+      let newdl_counter = 0;
+      try {
+        await async.forEachOfLimit(fetchData, 5, (value, key, callback) => {
           downloadFile(value, key).then(function (status) {
-            if (ISDEBUG) console.log("[DOWNLOAD] isDownload: " + status);
-            if (!status == true) newdl_counter++;
+            if (ISDEBUG && status) console.log("[DOWNLOAD] File OK: " + value.path);
+            if (!status) newdl_counter++;
             callback();
           });
-        })
-        //downloadFile(urls[4], null)
-        .then(() => {
-          if (ISDEBUG) console.log("[DOWNLOAD] Done - skipped:" + newdl_counter + " from: " + urls.length);
-          if (newdl_counter == urls.length && configSync) {
-            resolve("sync");
-          } else {
-            bars_multi.stop();
-            resolve(true);
-          }
-        })
-        .catch((err) => {
-          console.error("Download Error: " + err);
-          reject(err);
         });
+        if (ISDEBUG) console.log("[DOWNLOAD] done - skipped:" + newdl_counter + " from: " + fetchData.length);
+        resolve(true);
+      } catch (error) {
+        console.error("[DOWNLOAD] error: " + err);
+        reject(err);
+      }
     });
   },
   //Copy Temp Downloads to Live System
   sync: function () {
     if (fs.existsSync(UPDATE_FOLDER)) {
-      if (ISDEBUG) console.log("[SYNC] Begin");
+      if (ISDEBUG) console.log("[SYNC] begin");
       copyFolderRecursiveSync(UPDATE_FOLDER, LIVE_FOLDER);
     } else {
-      if (ISDEBUG) console.log("[SYNC] No Temp Folder - No Files to Sync");
+      if (ISDEBUG) console.log("[SYNC] no temp folder - no files to sync");
     }
   },
-  clean: function () {
+  clean: function (fetchData, configFile) {
+    if (ISDEBUG) console.log("[CLEAN] start");
     if (!fs.existsSync(LIVE_FOLDER)) {
       fs.mkdirSync(LIVE_FOLDER);
     }
-    fs.writeFileSync(LIVE_FOLDER + "/config.json", ConfigJSON);
     listFilesRecursive(LIVE_FOLDER, function (err, result) {
-      ParsedFiles.forEach((element) => {
-        let element_index = result.indexOf(BASEPATH + "/" + LIVE_FOLDER + url.parse(element).pathname);
-        if (element_index !== -1) {
+      console.log("[CLEAN] files in drive:");
+      console.log(result);
+      for (let index = 0; index < fetchData.length; index++) {
+        let element = BASEPATH + "/" + LIVE_FOLDER + "/" + fetchData[index].path;
+        let checkFile = result.includes(element);
+        if (checkFile) {
+          let element_index = result.indexOf(element);
           result.splice(element_index, 1);
-        } else {
-          console.log("[SYNC] Add to Ignor:" + element);
-          URL_IGNORES.push(element);
-          console.log("[SYNC] ERR File from JSON not found: " + LIVE_FOLDER + url.parse(element).pathname);
         }
-        var re = new RegExp(element, "g");
-        ConfigJSON = JSON.parse(JSON.stringify(ConfigJSON).replace(re, url.parse(element).pathname));
-      });
-      result.splice(result.indexOf(BASEPATH + "/" + LIVE_FOLDER + "/config.json"), 1);
-      result.splice(result.indexOf(BASEPATH + "/" + LIVE_FOLDER + "/config_files.json"), 1);
-      fs.writeFileSync(LIVE_FOLDER + "/config_files.json", ConfigJSON);
+      }
+      result.splice(result.indexOf(BASEPATH + "/" + LIVE_FOLDER + "/config.json", configFile), 1);
+      fs.writeFileSync(BASEPATH + "/" + LIVE_FOLDER + "/config.json", configFile);
       //Filter config.json not to delete
       deleteFiles(result, function (err) {
         if (err) {
@@ -421,7 +351,7 @@ export const configparser = {
   get_content_dir: function () {
     return LIVE_FOLDER;
   },
-  check_env_var: function (sysvar, altvar) {
-    return checkENV(sysvar, altvar);
+  check_env_var: function (sysvar, altvar, secret) {
+    return checkENV(sysvar, altvar, secret);
   },
 };
